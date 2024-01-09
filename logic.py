9e15388c6db3b781acce93d914ecc670f335ee76
Suvarna_Narayanan <suvarnanarayanan2g@gmail.com>
2024-01-09 11:26:25 +0530

    created enums for all the available datatypes

--- logic.py
+++ logic.py
@@ -1,6 +1,6 @@
 from faker import Faker
 from simple_ddl_parser import DDLParser
-from services import Table, AWSResponse
+from services import AWSResponse, Table
 
 """
 
@@ -16,7 +16,6 @@ Given sql DDL script, return list of
 
 
 def parse_ddl_script(ddl):
-
     # NOTE: A good design choice would be to separate the tables into difference scripts.
 
     try:
@@ -31,8 +30,6 @@ def parse_ddl_script(ddl):
 
     for elem in parse:
         tables.append(Table.from_json(elem))
-
-
 
     return parse
 
@@ -59,40 +56,6 @@ Given a list of
 
 """
 
+
 def generate_data():
-    fake = Faker()
-    for inst in tables:
-        for colinst in inst.columns:
-            types = str(colinst.type)
-            match types.upper():
-                case "INT":
-                    to_generate(fake.pyint(0, 2147483647), colinst.size)
-                case "TINYINT":
-                    to_generate(fake.pyint(0, 255), colinst.size)
-                case "BIT" | "BINARY" | "VARBINARY" | "IMAGE":
-                    to_generate(fake.pyint(0, 1), colinst.size)
-                case "SMALLINT":
-                    to_generate(fake.pyint(0, 32767), colinst.size)
-                case "MEDIUMINT":
-                    to_generate(fake.pyint(0, 8388607), colinst.size)
-                case "BIGINT":
-                    to_generate(fake.pyint(0, 9223372036854775808), colinst.size)
-                case "BOOL" | "BOOLEAN":
-                    to_generate(fake.pybool(), colinst.size)
-                case "VARCHAR" | "CHAR" | "TEXT" | "NVARCHAR" | "NCHAR" | "NTEXT":
-                    to_generate(fake.name(), colinst.size)
-                case "FLOAT" | "SMALLMONEY" | "MONEY" | "DECIMAL" | "DEC":
-                    to_generate(fake.pyfloat(0, 214748.3647), colinst.size)
-                case "DATE":
-                    to_generate(fake.date(), colinst.size)
-                case "DATETIME" | "DATETIME2" | "SMALLDATETIME":
-                    to_generate(fake.date_time_between(1753-1-1, 9999-12-31), colinst.size)
-                case "TIME":
-                    to_generate(fake.time(), colinst.size)
-                case "YEAR":
-                    to_generate(fake.year(), colinst.size)
-                case "TIMESTAMP":
-                    to_generate(fake.unix_time(), colinst.size)
-
-
-generate_data()
+    pass
